77a1670b18a244a42c97cbc478711f86349fcdfa
skrykg <skry_kg@mail.ru>
2020-01-29 13:51:03 +0600

    release v1

---
+++ app/engine/sait/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import FormView
+
+class MyRegisterFormView(FormView):
+    form_class = UserCreationForm
+    success_url = "/login/"
+    template_name = "register.html"
+
+    def form_valid(self, form):
+        form.save()
+        return super(MyRegisterFormView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        return super(MyRegisterFormView, self).form_invalid(form)
